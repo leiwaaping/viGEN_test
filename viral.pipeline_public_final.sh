ba2f676a67cd6ed92c53281adb1250cec5e28a10
leiwaaping <47714000+leiwaaping@users.noreply.github.com>
2019-04-30 11:32:26 +0800

    Add files via upload

--- viral.pipeline_public_final.sh
+++ viral.pipeline_public_final.sh
@@ -1,78 +1,82 @@
 #!/bin/bash
 
-# Alignment using Bowtie2
+# Align to viral reference using Bowtie2
 functionBowtie2() {
-	echo "Start - Align using Bowtie2 local algin" >> $LOG
-	$BOWTIE2_SW"bowtie2" -x $BOWTIE2_REF --local -1 $I_FOLDER$ID"_1.fq" -2 $I_FOLDER$ID"_2.fq" --threads $N_THREAD --al $O_FOLDER$ID/$ID".bowtie2.align" --un $O_FOLDER$ID/$ID".Bowtie2.unaligned" -S $O_FOLDER$ID/$ID".bowtie2.sam" --sensitive > $O_FOLDER$ID/$ERR2 2> $O_FOLDER$ID/$ERR3
-	echo "End - Align using Bowtie2 local algin" >> $LOG
+    echo "Start - Align to viral reference using Bowtie2 local algin"
+    bowtie2 -x ${BOWTIE2_REF} --local -1 ${I_FOLDER}${ID}_1.fastq -2 ${I_FOLDER}${ID}_2.fastq --threads ${N_THREAD} --al ${O_FOLDER}${ID}/${ID}.bowtie2.align --un ${O_FOLDER}${ID}/${ID}.Bowtie2.unaligned -S ${O_FOLDER}${ID}/${ID}.bowtie2.sam --sensitive
+    echo "End "
 }
 
 #Convert SAM file to BAM file
 functionConvertSamtoBam() {
-    echo "Start - convert sam to bam file using Samtools" >> $LOG
-	$SAMTOOLS"samtools" view -bS $O_FOLDER$ID/$ID".bowtie2.sam" > $O_FOLDER$ID/$ID".bowtie2.bam"
-    echo "End - convert sam to bam file using Samtools" >> $LOG
+    echo "Start - convert sam to bam file using Samtools" 
+#    samtools view -bS -@ $N_THREAD ${O_FOLDER}${ID}/${ID}.bowtie2.sam > ${O_FOLDER}${ID}/${ID}.bowtie2.bam &&
+    samtools view -bS -@ $N_THREAD ~/../../data/HPli_data/${ID}.bowtie2.sam > ${O_FOLDER}${ID}/${ID}.bowtie2.bam &&
+    echo "End " 
+    #save space
+    rm ${O_FOLDER}${ID}/${ID}.bowtie2.sam
 }
 
 #Sort BAM coordinate wise
 functionSortBam() {
-    echo "Start - Samtools sort bam file" >> $LOG
-	$SAMTOOLS"samtools" sort $O_FOLDER$ID/$ID".bowtie2.bam" -o $O_FOLDER$ID/$ID".bowtie2.sorted.bam" -@ $N_THREAD
-    echo "End - Samtools sort bam file" >> $LOG
-} 
+    echo "Start - Samtools sort bam file" 
+    samtools sort -@ $N_THREAD ${O_FOLDER}${ID}/${ID}.bowtie2.bam -o ${O_FOLDER}${ID}/${ID}.bowtie2.sorted.bam &&
+    echo "End " 
+    rm ${O_FOLDER}${ID}/${ID}.bowtie2.bam 
+}
 
 #Index BAM file
 functionIndexBam() {
-    echo "Start - Samtools index bam file" >> $LOG
-	$SAMTOOLS"samtools" index $O_FOLDER$ID/$ID".bowtie2.sorted.bam"
-    echo "End - Samtools index bam file" >> $LOG
+    echo "Start - Samtools index bam file"
+    samtools index -@ $N_THREAD ${O_FOLDER}${ID}/${ID}.bowtie2.sorted.bam &&
+    echo "End "
 }
 
 # Samtools idx -  genome level counts
 functionIdxStatsBam() {
-    echo "Start - Samtools Idx" >> $LOG
-	$SAMTOOLS"samtools" idxstats $O_FOLDER$ID/$ID".bowtie2.sorted.bam" > $O_FOLDER$ID/$ID".bowtie2.idxstats.txt"
-    echo "End - Samtools Idx" >> $LOG
+    echo "Start - Samtools Idx"
+    samtools idxstats -@ $N_THREAD ${O_FOLDER}${ID}/${ID}.bowtie2.sorted.bam > ${O_FOLDER}${ID}/${ID}.bowtie2.idxstats.txt &&
+    echo "End "
  }
 
 ########################################
 # Values to set by the user
-N_THREAD=4
-I_FOLDER="/Users/ls483/Documents/SRA.GEO/input/unmapped_fastq/"
-O_FOLDER="/Users/ls483/Documents/SRA.GEO/output_bowtie2/"
+N_THREAD=20
+I_FOLDER="~/../../data/HPli_data/SRA/"
+O_FOLDER="output/"
 
-LOG=$O_FOLDER"/pipeline_log.txt"
+LOG=$O_FOLDER$ID/"pipeline_log.txt"
 ERR2="system.err.bowtie2.txt"
 ERR3="log.bowtie2.txt"
-SAMTOOLS="/Users/ls483/Documents/software/samtools/bin/"
-BOWTIE2_SW="/Users/ls483/Documents/software/bowtie/bowtie2-2.2.9/"
-BOWTIE2_REF="/Users/ls483/Documents/SRA.GEO/viral.reference/virus.bowtie2.refB/virus.bowtie2.refB"
+#SAMTOOLS="~/.conda/envs/bioinenv/bin/"
+#BOWTIE2_SW="~/.conda/envs/bioinenv/bin/"
+BOWTIE2_REF="./3_Get.viral.reference/viral.bowtie2.ref/virus.bowtie2.ref"
 		
 for x in $(cat samplenames.txt)
 do
     echo $x
     ID=${x%*}
 
-    echo "-----------------------------" >> $LOG
-    echo $ID >> $LOG
+    echo "-----------------------------" 
+    echo "precessing :$ID" 
 
     START=$(date +%s) 
     
     mkdir $O_FOLDER$ID
 
-    functionBowtie2
+    functionBowtie2 &&
 
-    functionConvertSamtoBam
+    functionConvertSamtoBam &&
 
-    functionSortBam
+    functionSortBam &&
 
-    functionIndexBam
+    functionIndexBam &&
 
-    functionIdxStatsBam
+    functionIdxStatsBam &&
 
-    functionFlagstatBam
+    #functionFlagstatBam &&
     
-	END=$(date +%s)
-	DIFF=$(( ($END - $START)/60 )) 
-	echo "It took $DIFF minutes" >> $LOG
-done 
+    END=$(date +%s)
+    DIFF=$((($END-$START)/60))
+    echo "It took $DIFF minutes"
+done
